0b959deecfa055a2ae792da41747c84d0328e66f
rutu-sh <ruturajs10@hotmail.com>
2024-06-12 02:18:47 -0400

    feat: added logger for simple-restapi-server/python

---
+++ src/simple-restapi-server/python/src/common/config.py
@@ -0,0 +1,4 @@
+import os
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+LOG_TIME_FORMAT = f"%Y-%m-%d %H:%M:%S"
--- src/simple-restapi-server/python/src/main.py
+++ src/simple-restapi-server/python/src/main.py
@@ -1,12 +1,15 @@
 from controller import controller
+from common import common
 
+_logger = common.get_logger("test", bind_params={"param1": "value1", "param2": "value2"}, handlers=[common.logging.StreamHandler(), common.logging.FileHandler("log.txt")])
 
 def main():
-    pass
+    _logger.error("Starting the server", a=1, b=3)
+    _logger.warning("Starting the server")
+    _logger.debug("Starting the server")
+    _logger.info("Starting the server")
+    _logger.info("Server started")
 
 
 if __name__ == '__main__':
-    main()
-
-
-    
+    main() 
